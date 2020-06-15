ade84ed237b98be931940f8b7b5dee61945ac906
Anna <54270425+Balkanna@users.noreply.github.com>
2020-06-15 19:36:49 +0300

    Add favorites container

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const searchBtn = document.querySelector('.search__button'),
       search = document.querySelector('.search__input'),
-      resultsSection = document.querySelector('#results');
+      resultsSection = document.querySelector('#results'),
+      favoritesSection = document.querySelector('#favorites');
 
 function getFilms(search) {
     const key = 'd8090274';
@@ -24,22 +25,45 @@ searchBtn.addEventListener('click', (event) => {
 });
 
 function showResults(results) {
-    resultsSection.innerHTML = '';
-    let html = '';
-    results.forEach(movie => {
-        html += `
-        <div class="card col-3">
-            <h5 class="card-title">${movie.Title}</h5>
-            <img src="${movie.Poster}" class="card-img-top card-img" alt="${movie.Title}">
-            <div class="card-body">
-                <p class="card-text">(${movie.Year})</p>
-                <!--<a href="#" class="btn btn-primary">Watch</a>-->
-            </div>
-        </div>
-        `;
+    resultsSection.innerHTML = results.reduce((html, movie) => {
+        return html + getMovieTemplate(movie, 3);
+    }, '');
+
+    const favoritesButton = document.querySelectorAll('.favorites-button');
+
+    favoritesButton.forEach( button => {
+        button.addEventListener('click', (event) => {
+            const { id } = button.dataset;
+            const movie = results.find(movie => movie.imdbID === id);
+            favoritesSection.innerHTML = favoritesSection.innerHTML + getMovieTemplate(movie, 12, false);
+            button.parentNode.removeChild(button);
+        })
     });
-    resultsSection.innerHTML = html;
 }
 
+function removeFavorites() {
+    const removeButton = document.querySelectorAll('.remove-button');
+    removeButton.forEach( button => {
+        button.addEventListener('click', (event) => {
+            console.log('Clicked!');
+            favoritesSection.innerHTML += '';
+        })
+    });
+}
 
-
+function getMovieTemplate(movie, cols, button = true) {
+    return `
+    <div class="card col-${cols}">
+        <h5 class="card-title">${movie.Title}</h5>
+        <img src="${movie.Poster}" class="card-img-top card-img" alt="${movie.Title}">
+        <div class="card-body">
+            <p class="card-text">(${movie.Year})</p>
+            ${
+                button ? 
+                `<button data-id="${movie.imdbID}" type="button" class="btn btn-primary favorites-button">Add to favorites</button>`
+                : `<button data-id="${movie.imdbID}" type="button" class="btn btn-primary remove-button">Remove</button>`
+            }
+         </div>
+    </div>
+    `;
+};
